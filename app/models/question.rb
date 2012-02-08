a2f6a0c65981416fac2bb8ced188fb56ff7dcc70
Joe Burgess <joemburgess@gmail.com>
2012-02-08 12:01:33 -0500

    Add listing correct and incorrect users

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,29 @@
 class Question < ActiveRecord::Base
   has_many :answers, :dependent => :destroy
+  has_many :users, :through => :answers
   belongs_to :course
   accepts_nested_attributes_for :answers,  :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 
   def open?
     startdate<Time.now && enddate>Time.now
   end
-  
+
+  def correct_answers
+    answers.where("correct=?", true)
+  end
+
+  def correct_users
+    correct_user = Array.new
+    self.users.each do |user|
+      if user_is_correct?(user)
+        correct_user << user
+      end
+    end
+    correct_user
+  end
+
+  def user_is_correct?(user)
+    !(correct_answers & user.answers).empty?
+  end
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207071733) do
+ActiveRecord::Schema.define(:version => 20120208153825) do
 
   create_table "answers", :force => true do |t|
     t.string   "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120207071733) do
     t.datetime "enddate"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "course_id"
   end
 
   create_table "roles", :force => true do |t|
